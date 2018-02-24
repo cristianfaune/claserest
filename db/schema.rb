176bdfca01be54d0013d45ffeac1e6ce165a50a2
Cristian Faune <cristianfaune.t@gmail.com>
2018-02-24 13:05:29 -0300

    arreglando vistas

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180224131940) do
+ActiveRecord::Schema.define(version: 20180224143143) do
 
   create_table "cats", force: :cascade do |t|
     t.string   "name"
@@ -34,6 +34,9 @@ ActiveRecord::Schema.define(version: 20180224131940) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
+    t.integer  "age"
+    t.string   "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
